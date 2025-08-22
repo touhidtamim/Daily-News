af1bfa555dc93a5d6c705457847739010906f6a5
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-22 11:14:02 +0600

    updated home layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Daily News",
@@ -19,10 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="bg-white text-black">
+        <Navbar />
+        <main className="container mx-auto px-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
