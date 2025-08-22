b99e85a70b67e1e98d24d7c523346d3eb51866c7
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-22 12:24:43 +0600

    update data theme into light

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className="bg-white text-black">
         <Navbar />
         <main className="container mx-auto px-4">{children}</main>
